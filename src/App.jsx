fe1c966a9f0445852878f2fb1d245aa2685e39e5
mmicat <miramohd.pro@outlook.com>
2026-07-02 19:34:20 +0400

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>D&D 5e Monsters Dashboard</h1>
+      
+      <div className="summary-stats">
+        {}
+      </div>
+
+      <div className="search-and-filter">
+         {}
+      </div>
+
+      <div className="list-container">
+        {}
+      </div>
+    </div>
+  )
+}
+
+export default App
