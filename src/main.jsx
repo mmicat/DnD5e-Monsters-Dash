8b0e27f3c8d6ee845c6fa0437622ac3c137651a4
mmicat <miramohd.pro@outlook.com>
2026-07-09 15:42:57 +0400

    setup React Router, Recharts, base Layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter,Routes,Route } from 'react-router'
+import Layout from "./routes/Layout";
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   </StrictMode>,
 )
